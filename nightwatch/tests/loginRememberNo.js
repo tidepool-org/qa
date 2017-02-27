5e7f5b03319f819379bb2e5448df77be59cb8146
Ed Nykaza <edward.t.nykaza@gmail.com>
2017-02-26 19:33:17 -0600

    add logout to login test

--- nightwatch/tests/loginRememberNo.js
+++ nightwatch/tests/loginRememberNo.js
@@ -2,13 +2,15 @@
 
 module.exports = {loginRememberNo : function (browser) {
     
-    var userEmail = 'nightwatch+hasData1+' + process.env.TIDEPOOL_BLIP_USER_SKIP_KEY + '@tidepool.org';
+    var userEmail = 'nightwatch+hasData1+' +
+        process.env.TIDEPOOL_BLIP_USER_SKIP_KEY + '@tidepool.org';
     
     browser
         .url(process.env.TIDEPOOL_BLIP_LAUNCH_URL)
         .page.login().signIn(userEmail, false)
         .waitForElementPresent('.Navbar-loggedInAs')
         .pauseAndSaveScreenshot(10000, 'blip-logged-in-page')
+        .page.logout().logout()
         .end();
     }
 };
