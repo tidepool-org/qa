a11f5f30844f55fb438ae24540e384da09481b6a
Ed Nykaza <edward.t.nykaza@gmail.com>
2017-05-19 15:04:03 -0500

    test remember me checkbox workflow with refresh screen
    this test checks to make sure that if the remember be checkbox is NOT checked that the user has to log back in on a refresh of the URL, and that the user stays logged in on a refresh of the URL if they check the checkbox.

--- nightwatch/tests/testRememberMeRefreshURLWorkflow.js
+++ nightwatch/tests/testRememberMeRefreshURLWorkflow.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = {doNotRememberMeWorkflow : function (browser) {
+module.exports = {testRememberMeRefreshURLWorkflow : function (browser) {
 
     var staticUser = browser.globals.users.staticUser;
     
@@ -13,6 +13,13 @@ module.exports = {doNotRememberMeWorkflow : function (browser) {
         .url(process.env.TIDEPOOL_BLIP_LAUNCH_URL)
         .page.loginPage().confirmOnLoginPage()
         .pauseAndSaveScreenshot(10000, 'blip-login-page')
+        .page.loginPage().signInAndRememberMe(staticUser)
+        .waitForElementPresent('.Navbar-loggedInAs')
+        .pauseAndSaveScreenshot(10000, 'blip-logged-in-again-page')
+        .pause(10000)
+        .url(process.env.TIDEPOOL_BLIP_LAUNCH_URL)
+        .pause(10000)
+        .waitForElementPresent('.Navbar-loggedInAs')
         .end();
     }
 };
