dd8db9e7415c197695b7bf70c9a5b6bf4d74aa4a
Ed Nykaza <edward.t.nykaza@gmail.com>
2017-07-30 17:18:19 -0500

    get rid of unneeded variable

--- nightwatch/tests/loginVCAViewPatientData.js
+++ nightwatch/tests/loginVCAViewPatientData.js
@@ -1,13 +1,10 @@
-"use strict";
+'use strict';
 
 module.exports = {
     loginVCAViewPatientData : function (browser) {
-
-        var vca = browser.globals.users.verifiedClinician;
-
         browser
             .url(process.env.TIDEPOOL_BLIP_LAUNCH_URL)
-            .page.loginPage().signInAndRememberMe(vca)
+            .page.loginPage().signInAndRememberMe(browser.globals.users.verifiedClinician)
             .page.dataPage().confirmLoggedIn()
             .page.patientsPage().viewPatientData()
             .page.dataPage().confirmOnDataPage()
